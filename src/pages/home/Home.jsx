aa3fd2344a200701dbd96fc02823a2721f893864
Ki-fi <k.meekels@gmail.com>
2025-09-09 12:35:30 +0200

    Background image added and positioned

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,9 @@ function Home () {
     const navigate = useNavigate();
 
     return (
+        <>
         <div className="homepage">
+            <main className="home-container">
             <div className="greeting-container">
                 <img src="/src/assets/HELLO.svg" alt="Hello"/>
                 <img src="/src/assets/I’M KIKI.svg" alt="I'm Kiki"/>
@@ -25,7 +27,19 @@ function Home () {
                     buttonText="contact"
                 />
             </div>
+            </main>
+            <footer className="techstack-container">
+                <img src="/src/assets/tech-stack/Figma.png" alt="Figma"/>
+                <img src="/src/assets/tech-stack/HTML_.png" alt="HTML"/>
+                <img src="/src/assets/tech-stack/CSS.png" alt="CSS"/>
+                <img src="/src/assets/tech-stack/J_S.png" alt="Javascript"/>
+                <img src="/src/assets/tech-stack/React.png" alt="React"/>
+            </footer>
         </div>
+        <div className="background-container">
+            <img src="/src/assets/Keys.png" alt="Keyboard"/>
+        </div>
+        </>
     )
 }
 

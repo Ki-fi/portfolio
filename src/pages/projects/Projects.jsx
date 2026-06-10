b1adfe9a7515969b73c7932aa66753bf52e92992
Ki-fi <k.meekels@gmail.com>
2026-06-10 14:20:49 +0200

    Layout projects page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,14 +1,18 @@
 import './Projects.css';
+import Header from "../../components/header/Header.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+import Button from "../../components/button/Button.jsx";
+import portrait from "/src/assets/portrait.png";
 import ProjectCard from "../../components/project-card/ProjectCard.jsx";
 import thumbnailProject1 from "../../assets/petnet/Mockup_Post.jpg";
 import thumbnailProject2 from "../../assets/EP/Mockup_Overview.jpg";
 import thumbnailProject3 from "../../assets/PIDZ/Mockup_login.jpg";
 import Chip from "../../components/chip/Chip.jsx";
 import SEO from "../../components/SEO.jsx";
-// import {useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 function Projects() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
         <>
@@ -17,6 +21,10 @@ function Projects() {
             description="Mijn portfolio met projecten in User Research, Interaction Design, Visual Design en Development"
         />
         <div className="projects-page">
+            <header className="projects-header">
+                <Header/>
+            </header>
+            <main className="projects-wrapper">
             <div className="project-1">
                 <ProjectCard
                     thumbnail={thumbnailProject1}
@@ -50,6 +58,23 @@ function Projects() {
                         </>}
                 />
             </div>
+            </main>
+            <section className='contact-section'>
+                    <img className="contact-image" src={portrait} alt="avatar" />
+                    <span className='subheading'>Samenwerken?</span>
+                    <p>Ik ben benieuwd naar je idee of uitdaging en ik denk graag even met je mee.</p>
+                    <div className='button-container'>
+                        <Button
+                            type="button"
+                            variant="primary"
+                            onClick={() => {window.location.href = "mailto:k.meekels@gmail.com"}}>
+                            Plan een kennismaking
+                        </Button>
+                    </div>
+                </section>
+            <footer>
+                <Footer/>
+            </footer>
         </div>
         </>
 
